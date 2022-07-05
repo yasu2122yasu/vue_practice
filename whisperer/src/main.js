38da91c0717b5bd97252d50e8a7f99766e25c620
Yasushi Tamura <yasushitamura@YasushinoMacBook-Air.local>
2022-07-06 01:15:06 +0900

    Vueの導入

--- whisperer/src/main.js
+++ whisperer/src/main.js
@@ -1,5 +1,20 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import Vue from 'vue'
+import App from './App'
 import router from './router'
 
-createApp(App).use(router).mount('#app')
+// ここから追加
+import { library } from '@fortawesome/fontawesome-svg-core'
+import {
+  faUser,
+  faSignOutAlt,
+  faEllipsisV
+} from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(
+  faUser,
+  faSignOutAlt,
+  faEllipsisV
+)
+
+Vue.component('fa', FontAwesomeIcon)
